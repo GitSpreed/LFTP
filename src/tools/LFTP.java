8b4779bae96e917c202a7e5c46493d428ce056ab
spreed <1090329705@qq.com>
2018-12-04 11:34:27 +0800

    v0.2

--- src/tools/LFTP.java
+++ src/tools/LFTP.java
@@ -1,10 +1,16 @@
 package tools;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,10 +31,17 @@ public class LFTP extends Thread{
 	private MyList<Packet> list;
 	
 	private int cwnd, fwnd;
+	private int seqNum = 0;
 	private int ackNum = 0;
+	
+	private int lastByteRecv = 0;
+	
 	private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	
 	private connectionType ctype;
+	
+	private ArrayList<Packet> cache = new ArrayList<Packet>();
+	private InputStream in;
 	
 	public LFTP(InetAddress dstAddr, int srcPort, int dstPort, int UDPDstPort, Object listLock, MyList<Packet> list, Object socketLock, DatagramSocket socket, connectionType ctype) throws SocketException {
 		this.dstAddr = dstAddr;
@@ -57,18 +70,29 @@ public class LFTP extends Thread{
 				if (iter.getDstPort() == srcPort) {
 					temp = iter;
 					list.remove(iter);
+					return temp;
 				}
 			}
-			return temp;
+			return null;
 		}
 	}
 	
 	private void sendFile() {
-		
+		byte[] data = new byte[Packet.MAX_PACKET_LENGTH - Packet.MIN_PACKET_LENGTH];
+		int bytesNum = 0;
+		try {
+			while((lastByteRecv - seqNum) < Math.max(fwnd, cwnd) && (bytesNum = in.read(data)) != -1 ) {
+				Packet packet = new Packet(srcPort, dstPort, false, true, false, seqNum, ackNum, fwnd, Arrays.copyOf(data, bytesNum));
+				this.send(packet);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void sayHello() {
-		Packet packet = new Packet(srcPort, dstPort, true, false, false, (int)(1 + Math.random() * 1000), 0, fwnd, new byte[1]);
+		seqNum = (int)(1 + Math.random() * 1000);
+		Packet packet = new Packet(srcPort, dstPort, true, false, false, seqNum, 0, fwnd, new byte[1]);
 		try {
 			this.send(packet);
 		} catch (IOException e) {
@@ -77,11 +101,50 @@ public class LFTP extends Thread{
 	}
 	
 	private void replyHello() {
-		
+		seqNum = (int)(1 + Math.random() * 1000);
+		Packet packet = new Packet(srcPort, dstPort, true, true, false, seqNum, ackNum + 1, fwnd, new byte[1]);
+		try {
+			this.send(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void sendBack() {
-		
+		Packet packet = new Packet(srcPort, dstPort, false, true, isFinished, seqNum++, ackNum + 1, fwnd, new byte[1]);
+		try {
+			this.send(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean updateAckNum(int seq, int ack) {
+		if (seq == ackNum + 1) {
+			ackNum = ack;
+			Integer n = map.get(ackNum + 1);
+			while (n != null) {
+				ackNum = n;
+				n = map.get(ackNum + 1);
+				map.remove(ackNum + 1);
+			}
+		} else {
+			if (map.get(seq) != null) return false;
+			map.put(seq, ack);
+		}
+		return true;
+	}
+	
+	public void setFilePath(String path) {
+		File file = new File(path);
+		if (!file.exists()) {
+			isFinished = true;
+		}
+		try {
+			in = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void receiveFile() throws IOException {
@@ -96,7 +159,13 @@ public class LFTP extends Thread{
 				}
 			} else {
 				flag = false;
-				/*TODO write temp into file */
+				this.updateAckNum(packet.getSeqNum(), packet.getSeqNum() + packet.getData().length);
+				lastByteRecv = Math.max(lastByteRecv, packet.getAckNum());
+				if (ctype == connectionType.GET) {
+					isFinished = packet.isFIN();
+					this.sendBack();
+					/*TODO write cache into file */
+				}
 			}
 		}
 	}
@@ -105,10 +174,11 @@ public class LFTP extends Thread{
 	public void run() {
 		while (!isFinished) {
 			try {
-				this.sendFile();
+				if (ctype == connectionType.SEND) {
+					this.sendFile();
+				}	
 				this.receiveFile();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
